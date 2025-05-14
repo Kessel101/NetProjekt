115ded8c75ac68e3fba8ca445236c6ff1254944d
Michal Antosiewicz <antosiewicz.michal05@gmail.com>
2025-05-14 18:00:53 +0200

    EF Core polaczony z SQL serwer, chyba dziala ale glowy se nie dam urwa

---
+++ NetProject/Data/MyAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using NetProject.Models;
+
+namespace NetProject.Data
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSety: każda właściwość to tabelka w bazie
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        // … dodaj kolejne DbSet<T> dla innych modeli
+    }
+}
--- NetProject/Program.cs
+++ NetProject/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
+using NetProject.Data;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var cs = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MyAppDbContext>(opt =>
+    opt.UseSqlServer(cs));
+
+var app = builder.Build();
+
+// TEST: Endpoint, który sprawdzi połączenie z DB
+app.MapGet("/", async (MyAppDbContext db) =>
+{
+    var canConnect = await db.Database.CanConnectAsync();
+    return canConnect ? "Połączono z bazą danych!" : "Brak połączenia!";
+});
+
+app.Run();
